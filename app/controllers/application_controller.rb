58e93659ad0a1acbe6ff863ad48dceeedae7bd52
Javier Ponzo <ponzojavier@gmail.com>
2025-07-17 23:49:28 +0200

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_user_token
+
+  private
+  def set_user_token
+    session[:user_token] ||= SecureRandom.uuid
+  end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,21 @@
 class SearchController < ApplicationController
+  def index
+  end
+
+  def create
+    @query = params[:query]
+    user_token = session[:search_user_token]
+    last_query = SearchQuery.where(user_token: user_token).order(created_at: :desc).first
+
+    if last_query.nil? || last_query.query_text != @query
+      SearchQuery.create(user_token: user_token, query_text: @query)
+    end
+
+    head :ok
+  end
+
+  def analytics
+    user_token = session[:search_user_token]
+    @searches = SearchQuery.where(user_token: user_token).order(created_at: :desc)
+  end
 end
